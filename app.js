917ba10b3197e48ea105f88154f8a272e94064b6
Kasey Knudsen <kaseyknudsen@gmail.com>
2022-12-05 21:37:48 -0800

    Node callbacks and error handling

--- app.js
+++ app.js
@@ -2,23 +2,37 @@
 //   perimeter: (x, y) => 2 * (x + y),
 //   area: (x, y) => x * y,
 // };
-//require is a function from node. It reads a JS file, executes the file, and then 
+//require is a function from node. It reads a JS file, executes the file, and then
 //proceeds to return the exports object
-const rect = require('./rectangle')
+const rect = require("./rectangle");
 
 function solveRect(l, w) {
+    // 1)
   console.log(`Solving for rectangle with dimensions: ${l}, ${w}`);
-  if (l <= 0 || w <= 0) {
-    console.log(
-      `Rectangle dimensions must be greater than zero. Received: ${l}, ${w}`
-    );
-  } else {
-    console.log(`Area of rectangle: ${rect.area(l, w)}`);
-    console.log(`Perimeter of rectangle: ${rect.perimeter(l, w)}`);
-  }
+
+  rect(l, w, (err, rectangle) => {
+    if (err) {
+      console.log("ERROR:", err.message);
+    } else {
+        // 3)
+      console.log(`Area of rectangle with dimensions ${l}, ${w} is: ${rectangle.area()}`);
+    //   4)
+      console.log(`Perimeter of rectangle with dimensions ${l}, ${w}: ${rectangle.perimeter()}`);
+    }
+  });
+//   2)
+  console.log("This statement is logged after the call to rect()")
 }
 
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,5);
-solveRect(5,-3);
+solveRect(2, 4);
+// solveRect(3, 5);
+// solveRect(0, 5);
+// solveRect(5, -3);
+
+
+
+// this is how it executes:
+// Solving for rectangle with dimensions: 2, 4
+// This statement is logged after the call to rect()
+// Area of rectangle with dimensions 2, 4 is: 8
+// Perimeter of rectangle with dimensions 2, 4: 12
--- rectangle.js
+++ rectangle.js
@@ -1,6 +1,28 @@
-//exports is shorthand for module.exports
+//exports is shorthand for module.exports. YOu can use this when defining a method on the exports
 //this file is a module with 2 exports
 //exports is an object
 //we are created methods on the exports object
-exports.perimeter = (x, y) => 2 * (x + y);
-exports.area = (x, y) => x * y;
+
+//assign a function as the value for the entire export instead just a method of the export, so
+//we can't use the shorthand
+module.exports = (x, y, callback) => {
+  if (x <= 0 || y <= 0) {
+    //node error callback pattern is important!
+    //it is standard practice in node JS for async functions to return an error object as the first argument
+    //error is a global object
+    callback(
+      new Error(
+        `Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`
+      )
+    );
+  } else {
+    setTimeout(
+      () =>
+        callback(null, {
+          perimeter: () => 2 * (x + y),
+          area: () => x * y,
+        }),
+      2000
+    );
+  }
+};
